3ed4541845bad7b8e176a10654986c5692aa621e
Justin Hiltz <justin@justinhiltz.com>
2016-06-01 13:12:09 -0400

    Initial commit, copied from my Codepen

---
+++ script.js
@@ -0,0 +1,26 @@
+function searchWiki(query) {
+  $.ajax({
+    url: "http://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&action=query&list=search&srsearch="+query+"&callback=?",
+    contentType: "application/json; charset=utf-8",
+    async: false,
+    dataType: "jsonp",
+    success: function (data, textStatus, jqXHR) {
+      var results = data.query.search;
+      var html = '';
+
+      for (var i = 0; i < 10; i++) {
+        html += '<a href="https://en.wikipedia.org/wiki/'+results[i].title+'"target="_blank"><h3>'+results[i].title+'</h3></a>';
+        html += '<p>'+results[i].snippet+'</p>';
+      }
+
+    $("#results").html(html);
+    },
+    error: function (errorMessage) {
+      console.log("Some error");
+    }
+  })
+}
+
+$('#search').keyup(function() {
+  searchWiki($('#search').val());
+});
